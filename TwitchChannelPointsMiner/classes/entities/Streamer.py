807303a42e167aa7f16f80c6bad470c496d47c4c
Alessandro Maggio <alex.tkd.alex@gmail.com>
2021-02-02 22:39:23 +0100

    Skip some events for annotations points. Only WATCH_STREAK WIN LOSE

--- TwitchChannelPointsMiner/classes/entities/Streamer.py
+++ TwitchChannelPointsMiner/classes/entities/Streamer.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import os
+import random
 import time
 from threading import Lock
 
@@ -107,54 +108,46 @@ class Streamer(object):
     def stream_up_elapsed(self):
         return self.stream_up == 0 or ((time.time() - self.stream_up) > 120)
 
-    def persistent_history(self):
-        fname = os.path.join(Settings.analytics_path, f"{self.username}.json")
-        with self.mutex:
-            data = json.load(open(fname)) if os.path.isfile(fname) else []
-            # https://stackoverflow.com/questions/4676195/why-do-i-need-to-multiply-unix-timestamps-by-1000-in-javascript
-            data.append([round(time.time() * 1000), self.channel_points])
-            with open(fname, "w") as outfile:
-                json.dump(data, outfile, indent=4)
-
     def persistent_points(self, event_type, event_text):
         event_type = event_type.upper()
-        primary_color = (
-            "#45c1ff"
-            if event_type == "WATCH_STREAK"
-            else ("#ff4560" if event_type == "LOSE" else "#54ff45")
-        )
-        data = {
-            "y": self.channel_points,
-            "marker": {
-                "size": 4,
-                "fillColor": "#fff",
-                "strokeColor": primary_color,
-                "radius": 2,
-            },
-            "label": {
-                "borderColor": primary_color,
-                "offsetY": 0,
-                "style": {
-                    "color": "#fff",
-                    "background": primary_color,
+        if event_type in ["WATCH_STREAK", "WIN", "LOSE"]:
+            primary_color = (
+                "#45c1ff"
+                if event_type == "WATCH_STREAK"
+                else ("#ff4560" if event_type == "LOSE" else "#54ff45")
+            )
+            data = {
+                "y": self.channel_points,
+                "marker": {
+                    "size": 4,
+                    "fillColor": "#fff",
+                    "strokeColor": primary_color,
+                    "radius": 2,
                 },
-                "text": event_text,
-            },
-        }
-        self.__save_json("points", data)
+                "label": {
+                    "borderColor": primary_color,
+                    "offsetY": 0,
+                    "style": {
+                        "color": "#fff",
+                        "background": primary_color,
+                    },
+                    "text": event_text,
+                },
+            }
+            self.__save_json("points", data)
 
     def persistent_series(self):
         self.__save_json("series", {"y": self.channel_points})
 
     def __save_json(self, key, new_data):
+        time.sleep(random.uniform(1, 3))
         fname = os.path.join(Settings.analytics_path, f"{self.username}.json")
         with self.mutex:
-            data = json.load(open(fname)) if os.path.isfile(fname) else {}
+            data = json.load(open(fname), "r") if os.path.isfile(fname) else {}
             if key not in data:
                 data[key] = []
 
             # https://stackoverflow.com/questions/4676195/why-do-i-need-to-multiply-unix-timestamps-by-1000-in-javascript
             new_data.update({"x": round(time.time() * 1000)})
             data[key].append(new_data)
-            with open(fname, "w") as outfile:
-                json.dump(data, outfile, indent=4)
+            json.dump(data, open(fname, "w"), indent=4)
