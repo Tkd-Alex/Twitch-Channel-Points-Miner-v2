a47355aec5db07ddc39f33b8551e5852e29c2f3f
dmunozv04 <39565245+dmunozv04@users.noreply.github.com>
2021-03-13 21:53:03 +0100

    Add baseurl support to analytics
    Adding baseurl support to analytics server

--- TwitchChannelPointsMiner/classes/AnalyticsServer.py
+++ TwitchChannelPointsMiner/classes/AnalyticsServer.py
@@ -20,17 +20,19 @@ def streamers_available():
     ]
 
 
-def read_json(streamer):
+def read_json(streamer, baseurl):
     path = Settings.analytics_path
     streamer = streamer if streamer.endswith(".json") else f"{streamer}.json"
+    streamer_without_baseurl = streamer.replace(baseurl, '')
     return Response(
-        open(os.path.join(path, streamer)) if streamer in streamers_available() else [],
+        open(os.path.join(path, streamer_without_baseurl)) if streamer in streamers_available() else [],
         status=200,
         mimetype="application/json",
     )
 
 
-def index(refresh=5):
+def index(refresh=5, baseurl=""):
+    baseurl = baseurl[1:]
     return render_template(
         "charts.html",
         refresh=(refresh * 60 * 1000),
@@ -38,25 +40,26 @@ def index(refresh=5):
     )
 
 
+
 class AnalyticsServer(Thread):
-    def __init__(self, host: str = "127.0.0.1", port: int = 5000, refresh: int = 5):
+    def __init__(self, host: str = "127.0.0.1", port: int = 5000, refresh: int = 5, baseurl: str = ""):
         super(AnalyticsServer, self).__init__()
 
         self.host = host
         self.port = port
         self.refresh = refresh
+        self.baseurl = baseurl
 
         self.app = Flask(
             __name__,
             template_folder=os.path.join(Path().absolute(), "assets"),
-            static_folder=os.path.join(Path().absolute(), "assets"),
+            static_folder=os.path.join(Path().absolute(), "assets"), static_url_path=f"{baseurl}/static",
         )
-        self.app.add_url_rule("/", "index", index, defaults={"refresh": refresh})
-        self.app.add_url_rule("/json/<string:streamer>", "json", read_json)
-
+        self.app.add_url_rule(f"{baseurl}/", "index", index, defaults={"refresh": refresh, "baseurl": baseurl})
+        self.app.add_url_rule(f"{baseurl}/json/<string:streamer>", "json", read_json, defaults={"baseurl": baseurl})
     def run(self):
         logger.info(
-            f"Analytics running on http://{self.host}:{self.port}/",
+            f"Analytics running on http://{self.host}:{self.port}{self.baseurl}",
             extra={"emoji": ":globe_with_meridians:"},
         )
         self.app.run(host=self.host, port=self.port, threaded=True)
