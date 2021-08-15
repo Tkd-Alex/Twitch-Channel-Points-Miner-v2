fa72feb21b51ab2507192fa9cc76a41a8f144b25
Alessandro Maggio <alex.tkd.alex@gmail.com>
2021-08-15 22:47:33 +0200

    Move javascript part to script.js - Implement orderBy feature

--- TwitchChannelPointsMiner/classes/AnalyticsServer.py
+++ TwitchChannelPointsMiner/classes/AnalyticsServer.py
@@ -21,24 +21,38 @@ def streamers_available():
     ]
 
 
-def read_json(streamer):
+def read_json(streamer, return_response=True):
     path = Settings.analytics_path
     streamer = streamer if streamer.endswith(".json") else f"{streamer}.json"
-    return Response(
-        open(os.path.join(path, streamer)) if streamer in streamers_available() else [],
-        status=200,
-        mimetype="application/json",
+    if return_response is True:
+        return Response(
+            open(os.path.join(path, streamer))
+            if streamer in streamers_available()
+            else [],
+            status=200,
+            mimetype="application/json",
+        )
+    return (
+        json.loads(open(os.path.join(path, streamer)).read())
+        if streamer in streamers_available()
+        else []
     )
 
 
+def get_challenge_points(streamer):
+    datas = read_json(streamer, return_response=False)
+    if datas != {}:
+        return datas["series"][-1]["y"]
+    return 0
+
+
 def json_all():
-    path = Settings.analytics_path
     return Response(
         json.dumps(
             [
                 {
                     "name": streamer.strip(".json"),
-                    "data": json.load(open(os.path.join(path, streamer))),
+                    "data": read_json(streamer, return_response=False),
                 }
                 for streamer in streamers_available()
             ]
@@ -52,7 +66,19 @@ def index(refresh=5):
     return render_template(
         "charts.html",
         refresh=(refresh * 60 * 1000),
-        streamers=",".join(streamers_available()),
+    )
+
+
+def streamers():
+    return Response(
+        json.dumps(
+            [
+                {"name": s, "points": get_challenge_points(s)}
+                for s in sorted(streamers_available())
+            ]
+        ),
+        status=200,
+        mimetype="application/json",
     )
 
 
@@ -70,6 +96,7 @@ class AnalyticsServer(Thread):
             static_folder=os.path.join(Path().absolute(), "assets"),
         )
         self.app.add_url_rule("/", "index", index, defaults={"refresh": refresh})
+        self.app.add_url_rule("/streamers", "streamers", streamers)
         self.app.add_url_rule("/json/<string:streamer>", "json", read_json)
         self.app.add_url_rule("/json_all", "json_all", json_all)
 
