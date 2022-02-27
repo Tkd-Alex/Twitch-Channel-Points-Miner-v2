fb1747a1dd7f69adf0c9dda91f004ef9c757d1cf
Prograstinator <99142422+prograstinator@users.noreply.github.com>
2022-02-27 13:17:01 +0000

    black

--- TwitchChannelPointsMiner/logger.py
+++ TwitchChannelPointsMiner/logger.py
@@ -138,11 +138,7 @@ class GlobalFormatter(logging.Formatter):
                 )
 
         if hasattr(record, "event"):
-            skip_discord = (
-                False
-                if hasattr(record, "skip_discord") is False
-                else True
-            )
+            skip_discord = False if hasattr(record, "skip_discord") is False else True
             if self.settings.discord is not None and skip_discord is False:
                 self.settings.discord.send(record.msg, record.event)
 
