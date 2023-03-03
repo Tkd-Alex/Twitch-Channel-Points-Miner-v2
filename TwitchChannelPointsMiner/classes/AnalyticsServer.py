c826df7a05562fafc10ca1319bfcc761e06a53b5
silentguy256 <silentguy@gmail.com>
2023-03-03 17:01:00 +0100

    Removed an aggregate that introduced another weird step But am unsure if it was there for an obscure reason

--- TwitchChannelPointsMiner/classes/AnalyticsServer.py
+++ TwitchChannelPointsMiner/classes/AnalyticsServer.py
@@ -56,7 +56,7 @@ def filter_datas(start_date, end_date, datas):
         df["datetime"] = pd.to_datetime(df.x // 1000, unit="s")
 
         df = df[(df.x >= start_date) & (df.x <= end_date)]
-        df = aggregate(df)
+
 
         datas["series"] = (
             df.drop(columns="datetime")
