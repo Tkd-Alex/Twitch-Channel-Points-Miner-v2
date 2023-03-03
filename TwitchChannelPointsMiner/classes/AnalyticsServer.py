cb87853ed1d315f1e9f77951ba95e04efe468aa1
silentguy256 <silentguy@gmail.com>
2023-03-03 16:38:17 +0100

    Got two values the wrong way round

--- TwitchChannelPointsMiner/classes/AnalyticsServer.py
+++ TwitchChannelPointsMiner/classes/AnalyticsServer.py
@@ -60,7 +60,7 @@ def filter_datas(start_date, end_date, datas):
 
         datas["series"] = (
             df.drop(columns="datetime")
-            .sort_values(by=["y", "x"], ascending=True)
+            .sort_values(by=["x", "y"], ascending=True)
             .to_dict("records")
         )
     else:
