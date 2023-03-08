2873b963368751c0cb80aca096b0bb8a0ae4cabb
Roman Davydov <15850461+rdavydov@users.noreply.github.com>
2023-03-08 18:17:44 +0300

    fix #189

--- TwitchChannelPointsMiner/TwitchChannelPointsMiner.py
+++ TwitchChannelPointsMiner/TwitchChannelPointsMiner.py
@@ -94,6 +94,25 @@ class TwitchChannelPointsMiner:
         # This disables certificate verification and allows the connection to proceed, but also makes it vulnerable to man-in-the-middle (MITM) attacks.
         Settings.disable_ssl_cert_verification = disable_ssl_cert_verification
 
+        import socket
+
+        def is_connected():
+            try:
+                # resolve the IP address of the Twitch.tv domain name
+                socket.gethostbyname("twitch.tv")
+                return True
+            except OSError:
+                pass
+            return False
+
+        # check for Twitch.tv connectivity every 5 seconds
+        error_printed = False
+        while not is_connected():
+            if not error_printed:
+                logger.error("Waiting for Twitch.tv connectivity...")
+                error_printed = True
+            time.sleep(5)
+
         # Analytics switch
         Settings.enable_analytics = enable_analytics
 
