3e76cbe2659b7a70cc96fc7c0d921eb9cf5b7acf
Roman Davydov <15850461+rdavydov@users.noreply.github.com>
2023-03-15 02:57:45 +0300

    auto_clear + time_zone

--- example.py
+++ example.py
@@ -26,6 +26,8 @@ twitch_miner = TwitchChannelPointsMiner(
         save=True,                              # If you want to save logs in a file (suggested)
         console_level=logging.INFO,             # Level of logs - use logging.DEBUG for more info
         console_username=False,                 # Adds a username to every console log line if True. Useful when you have many open consoles with different accounts
+        auto_clear=True,                        # Create a file rotation handler with interval = 1D and backupCount = 7 if True (default)
+        time_zone="",                           # Set a specific time zone for console and file loggers. Use tz database names. Example: "America/Denver"
         file_level=logging.DEBUG,               # Level of logs - If you think the log file it's too big, use logging.INFO
         emoji=True,                             # On Windows, we have a problem printing emoji. Set to false if you have a problem
         less=False,                             # If you think that the logs are too verbose, set this to True
