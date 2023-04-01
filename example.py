51e4e2cc6306100c7a335c47b5c3bcc175719186
Spencer <36741900+Spencer-0003@users.noreply.github.com>
2023-04-01 02:15:46 +0000

    Example(Matrix): Added events.

--- example.py
+++ example.py
@@ -49,10 +49,11 @@ twitch_miner = TwitchChannelPointsMiner(
             events=[Events.STREAMER_ONLINE, Events.STREAMER_OFFLINE, Events.BET_LOSE],       # Only these events will be sent to the chat
         ),
         matrix=Matrix(
-            username="twitch_miner", # Matrix username (without homeserver)
-            password="...",          # Matrix password
-            homeserver="matrix.org", # Matrix homeserver
-            room_id="..."            # Room ID
+            username="twitch_miner",                                                   # Matrix username (without homeserver)
+            password="...",                                                            # Matrix password
+            homeserver="matrix.org",                                                   # Matrix homeserver
+            room_id="..."                                                              # Room ID
+            events=[Events.STREAMER_ONLINE, Events.STREAMER_OFFLINE, Events.BET_LOSE], # Only these events will be sent to the chat
         )
     ),
     streamer_settings=StreamerSettings(
