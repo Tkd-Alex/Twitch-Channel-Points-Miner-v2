8596d2e30acb1c40672e2abeef144f3df2e1a3eb
Roman Davydov <15850461+rdavydov@users.noreply.github.com>
2023-07-21 13:59:35 +0300

    new hash for DropsHighlightService_AvailableDrops

--- TwitchChannelPointsMiner/constants.py
+++ TwitchChannelPointsMiner/constants.py
@@ -152,7 +152,7 @@ class GQLOperations:
         "extensions": {
             "persistedQuery": {
                 "version": 1,
-                "sha256Hash": "e589e213f16d9b17c6f0a8ccd18bdd6a8a6b78bc9db67a75efd43793884ff4e5",
+                "sha256Hash": "9a62a09bce5b53e26e64a671e530bc599cb6aab1e5ba3cbd5d85966d3940716f",
             }
         },
     }
